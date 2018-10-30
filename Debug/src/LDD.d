b69829a5e60439c13692b00a5e8b0e28227a4a49
root <Szymon.malczynski@student.wat.edu.pl>
2018-10-30 08:34:07 +0000

    Commit zadania 6

---
+++ Debug/src/LDD.d
@@ -0,0 +1,1 @@
+src/LDD.o: ../src/LDD.c
--- src/LDD.c
+++ src/LDD.c
@@ -12,6 +12,35 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello szymon!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello szyjjmon!!"); /* prints !!!Hello World!!! */
+
+	unsigned int * wsk_do_bufora;
+	unsigned int liczba_elementow_bufora=6;
+
+	wsk_do_bufora = malloc(liczba_elementow_bufora);
+
+
+	for(int n=0;n<liczba_elementow_bufora;n++)
+	{
+
+		wsk_do_bufora[n]=100+n;
+		printf("\n Element bufora = %d",wsk_do_bufora[n]);
+
+	}
+
+		printf("\nWyswietlam rozmiar wsk_do_bufora %d",sizeof(wsk_do_bufora));
+		printf("\n Wyswietlam rozmiar liczby elementow bufora %d",sizeof(liczba_elementow_bufora));
+
+		free(wsk_do_bufora);
+
+		for(int n=0;n<liczba_elementow_bufora;n++)
+			{
+
+
+				printf("\n Element bufora = %d",wsk_do_bufora[n]);
+
+			}
+
+
 	return EXIT_SUCCESS;
 }
